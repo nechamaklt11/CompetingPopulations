c746a9d1e0ef579500db0d7f6f06bf1a16ad6b7b
nechamaklt11 <48054996+nechamaklt11@users.noreply.github.com>
2021-01-13 01:50:05 +0200

    Update dnf_getdata.m

--- dnf_getdata.m
+++ dnf_getdata.m
@@ -1,4 +1,4 @@
-function Outstruct = dnf_getdata(Instruct) 
+function Outstruct = dnf_getdata1f(Instruct)
 %Section 2F
 %%%%%%%%%%%%  To DO   %%%%%%%%
 Titles = {'Maximal number of timepoints', 'Number of numSteps','Minimal population size', 'N0', 'Fk'};
@@ -7,14 +7,13 @@ Dims = [1, 30];
 while true
     Ans = inputdlg(Titles,'Simulations Data', Dims, Defaults); %display a dialog box where the user inserts values for the different fields
     Outstruct = struct('maxSteps', str2double(Ans{1}), 'numSteps', str2double(Ans{2}),...
-        'minSize', str2double(Ans{3}), 'N0', str2double(Ans{4}), 'Fk', str2double(Ans{5}));
+        'minSize', str2double(Ans{3}), 'N0', double(str2num(Ans{4})), 'Fk', str2double(Ans{5}));
     C=struct2cell(Outstruct); %arrange the struct data as a cell
     if all(~isnan([C{:}])) && all([C{:}]>0) %if all the inputs were inserted properly
         break
-    else % if the user inserted a negative/not numeric input
+      else % if the user inserted a negative/not numeric input
         Defaults=Ans; % in case the user corrected only one of a few illegal fields
         idx=[find(isnan([C{:}])),find([C{:}]<0)]; %find illegal inputs
         [Defaults(idx)]={'error: please insert positive numbers only'}; %display an error message
     end
 end
-
