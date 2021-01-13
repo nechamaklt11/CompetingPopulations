d54c86d82ea6341d3bdcf198e85136cd10f5c54c
nechamaklt11 <48054996+nechamaklt11@users.noreply.github.com>
2021-01-13 02:28:28 +0200

    Update tst.m

--- tst.m
+++ tst.m
@@ -17,7 +17,7 @@ Times=linspace(0,5,201);
 %%
 %QUESTION 2 section I ii 
 S1=struct('N0',5.1,'K',10.2,'lamda',1.11,'alpha',1.03);% create parameters struct for the 1st species
-S2=struct('N0',4,'K',10.5,'lamda',0.99,'alpha',1.09); %creates parameters struct for the 2nd species
+S2=struct('N0',4.7,'K',10.5,'lamda',1.04,'alpha',1.09); %creates parameters struct for the 2nd species
 S=[S1,S2]; %combine the two structs into one array. %%check if it is changed
 %%%% to do section iii
 popSize1=dnf_calcLogGrowth(S1,Times);
@@ -25,6 +25,8 @@ popSize2=dnf_calcLogGrowth(S2,Times);
 %section I v
 subplot(2,1,1)
 plot(popSize1,'k')
-subplot(2,1,2)
+hold on
 plot(popSize2,'k') %ask the metarglim if they mixed upper and lower subplots
+subplot(2,1,2)
+
 
