b3796ee422e7335589b6529fe3dee59f5c2c2b03
nechamaklt11 <48054996+nechamaklt11@users.noreply.github.com>
2021-01-19 01:00:58 +0200

    Add files via upload

--- dnf_knownSim.m
+++ dnf_knownSim.m
@@ -8,13 +8,35 @@ pop2 = nan(instruct2.maxSteps, instruct2.numRepeats);
 pop2Data = instruct1(2); %Calling the struct containing the data for species 2 into its own variable
 pop2(1,:) = pop2Data.N0;
 
+for i=1:instruct2.maxSteps
+    nextGen1=calcNextGen(instruct1(1),pop1(i,:),pop2(i,:));
+    nextGen2=calcNextGen(instruct1(2),pop2(i,:),pop1(i,:));
+    if instruct2.numRepeats>1
+        nextGen1=randomRound(nextGen1);
+        nextGen2=randomRound(nextGen2);
+        nextGen1(nextGen1<instruct2.minSize)=0;
+        nextGen2(nextGen1<instruct2.minSize)=0;
+        if all(nextGen1==0) || all(nextGen2==0)
+            break
+        end
+    end
+    pop1(i+1,:)=nextGen1;
+    pop2(i+1,:)=nextGen2;
+end
+pop1(isnan(pop1))=[];
+pop2(isnan(pop2))=[];
+Outstruct.Pop1=pop1; Outstruct.Pop2=pop2;
+Times=0:size(pop1,2);
+Outstruct.Times=Times';
+
 function nextGen = calcNextGen(popData,lastGen,compLastGen) %Function to calculate the population in the next generation
 nextGen = lastGen.*popData.lamda.^(1-((lastGen+popData.alpha.*compLastGen)./popData.K));
+
 function randOut = randomRound(Num) %Function to randomly round a number to a whole
 fractional = Num - floor(Num); %Finding the fractional part of each number
 randArray = rand(size(Num)); %Create a random array of fractions between 0 and 1
 roundUp = fractional>randArray;%Find positions of fractional parts larger than the random array
 roundDown = fractional<randArray; %Find positions of fractional parts smaller than the random array
 randOut = Num;
-randOut(roundUp) = round(Num(roundUp)); %Replace the original numbers with the ones round up
+randOut(roundUp) = ceil(Num(roundUp)); %Replace the original numbers with the ones round up
 randOut(roundDown) = floor(Num(roundDown)); %Replace the original numbers with the ones rounded down
--- tst.m
+++ tst.m
@@ -13,20 +13,39 @@ defParams=struct('maxSteps',50,'numRepeats',10,'minSize',2,...
 simParams=dnf_getdata(defParams);
 %%
 %QUESTION 2 SECTION I i
-Times=linspace(0,5,201);
-%%
-%QUESTION 2 section I ii 
-S1=struct('N0',5.1,'K',10.2,'lamda',1.11,'alpha',1.03);% create parameters struct for the 1st species
-S2=struct('N0',4.7,'K',10.5,'lamda',1.04,'alpha',1.09); %creates parameters struct for the 2nd species
-S=[S1,S2]; %combine the two structs into one array. %%check if it is changed
-%%%% to do section iii
-popSize1=dnf_calcLogGrowth(S1,Times);
-popSize2=dnf_calcLogGrowth(S2,Times);
+Times=linspace(0,5,201); 
+
+popParams=[struct('N0',5.1,'K',10.2,'lamda',1.11,'alpha',1.03),...
+    struct('N0',4.7,'K',10.5,'lamda',1.04,'alpha',1.4)]; %parameters for the two populations 
+%section 1 iii
+popParams(1).N0=simParams.N0(1); popParams(2).N0=simParams.N0(2); 
+%section i iv
+popSize1=dnf_calcLogGrowth(popParams(1),Times);
 %section I v
+f2=figure;
 subplot(2,1,1)
+%section I vi
 plot(popSize1,'k')
 hold on
-plot(popSize2,'k') %ask the metarglim if they mixed upper and lower subplots
+%section I vii
+popSize2=dnf_calcLogGrowth(popParams(2),Times);
+plot(popSize2,'k') 
+%%
+%SECTION K
+%i
+dtr_sim=dnf_knownSim(popParams,simParams); %determinstic simulation
+figure
+subplot(2,1,1)
+plot(dtr_sim.Pop1)
 subplot(2,1,2)
+plot(dtr_sim.Pop2)
 
+popParams(1).alpha=0; popParams(2).alpha=0; 
+simParams.minSize=0; simParams.numRepeats=1;
+stc_sim=dnf_knownSim(popParams,simParams); %stochastic simulation
 
+figure(f2)
+subplot(2,1,2)
+hold on
+plot(stc_sim.Pop1,'b:')
+plot(stc_sim.Pop2,'b:')
