419eb2e21191066d2624a1181a879d940f50f279
nechamaklt11 <48054996+nechamaklt11@users.noreply.github.com>
2021-02-27 23:25:32 +0200

    Add files via upload

--- dnf_evalLogParams.m
+++ dnf_evalLogParams.m
@@ -10,7 +10,6 @@ t_Idx=find(mat(:,2)<t); %find which elements meat the required criterion (t).
 times=mat(t_Idx,1); pop=mat(t_Idx,2); %choose the corresponding elements from the orginal matrice.
 C=[times ones(size(times))]; %create the "C" matrice
 [ab,ci] = regress(log(pop),C); %logarithmic linear regression 
-%do we need to change the confidence interval? 
 c=exp(ab); CI=exp(ci); 
 lamda=c(1); N0=c(2); CI_N0=CI(2,:); CI_lamda=CI(1,:); %function outputs
 
--- populations.m
+++ populations.m
@@ -1,5 +1,5 @@
 %QUESTION 3
-
+%%
 %section A
 clear; close all; %i
 fileData=dnf_getfile; %ii
@@ -7,6 +7,13 @@ lineProp={'ro','bo'}; %iii %array or a cell?  CONTRADICTION
 fig1=figure;
 dnf_plotPop(fileData,lineProp); %iv
 dnf_Annotate(fileData) %v
+%%
+%section E
+[pop1Params,error1Params]=dnf_calcSepParams(fileData.Pop1,0.8);%what is p? %i
+[pop2Params,error2Params]=dnf_calcSepParams(fileData.Pop2,0.8);%what is p? %ii
+K1=pop1Params.K; K2=pop2Params.K; %k values (i+ii); 
+sepPopParams=[pop1Params, pop2Params]; %estimated paramaters for both population (given separate populations)
+sepPopCI=[error1Params,error2Params]; %error calues for both populations, given separate populations. 
 
 
 
