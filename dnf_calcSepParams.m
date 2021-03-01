0ad9b0b5a420af5099b43f1dc28286435e0f98ce
Dor-Breier <77353410+Dor-Breier@users.noreply.github.com>
2021-03-01 20:24:40 +0200

    Update dnf_calcSepParams.m

--- dnf_calcSepParams.m
+++ dnf_calcSepParams.m
@@ -7,7 +7,7 @@ function [popParams,errorParams]=dnf_calcSepParams(inMat,p)
 %ouputs: popParams: one population params, errorParams: Confidence
 %intervals
 [convVal,CI_K,convIdx]=dnf_asympt(inMat,0.01); %convVal-convergence constant,CI_K-K confidence interval,convIdx-convergence index
-tsd=p*convVal; %logaritmic threshold 
+tsd=p*convVal; %logarithmic threshold 
 [lamda,N0,CI_lamda,CI_N0]=dnf_evalLogParams(inMat,tsd);%estimate No and lamda values
 popParams=struct('N0',N0,'K',convVal,'lamda',lamda,'alpha',0); %struct as described in 1)E)ii
 errorParams=struct('N0',CI_N0,'K',CI_K,'lamda',CI_lamda,'alpha',0);
