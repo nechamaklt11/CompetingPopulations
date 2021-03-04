58df15de9069f7d1d0823a9c96bdee9566cc7ae0
nechamaklt11 <48054996+nechamaklt11@users.noreply.github.com>
2021-03-05 00:23:58 +0200

    Add files via upload

--- dnf_calcSepParams.m
+++ dnf_calcSepParams.m
@@ -1,17 +1,17 @@
-function [popParams,errorParams]=dnf_calcSepParams(inMat,p)
+function [popParams,errorParams,convIdx]=dnf_calcSepParams(inMat,p)
 %QUESTION 2 SECTION D
 %calculate system parameters for separate populations. 
 %inputs: inMat: mat with two columns: time and population.
          %p: value represents the percentage that behind it the population
          %grows logarithmically. 
 %ouputs: popParams: one population params, errorParams: Confidence
-%intervals
+%intervals,convIdx:convergence index
 [convVal,CI_K,convIdx]=dnf_asympt(inMat,0.01); %convVal-convergence constant,CI_K-K confidence interval,convIdx-convergence index
 tsd=p*convVal; %logarithmic threshold 
 [lamda,N0,CI_lamda,CI_N0]=dnf_evalLogParams(inMat,tsd);%estimate No and lamda values
 popParams=struct('N0',N0,'K',convVal,'lamda',lamda,'alpha',0); %struct as described in 1)E)ii
 errorParams=struct('N0',CI_N0,'K',CI_K,'lamda',CI_lamda,'alpha',0);
-
+convIdx=convIdx(1);
 
 
          
