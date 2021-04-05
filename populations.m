aa43605502a88c2fd86318a413b99242da76bc64
nechamaklt11 <48054996+nechamaklt11@users.noreply.github.com>
2021-04-05 15:52:56 +0300

    Update populations.m

--- populations.m
+++ populations.m
@@ -3,7 +3,7 @@
 %section A
 clear; close all; %i
 fileData=dnf_getfile; %ii
-lineProp={'ro','bo'}; %iii %array or a cell?  CONTRADICTION
+lineProp={'ro','bo'}; %iii 
 fig1=figure;
 dnf_plotPop(fileData,lineProp); %iv
 dnf_Annotate(fileData) %v
