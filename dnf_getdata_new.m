a00ff8226e694e07b7949f8b18767b9f80a01b30
nechamaklt11 <48054996+nechamaklt11@users.noreply.github.com>
2021-04-05 13:42:37 +0300

    Add files via upload

---
+++ dnf_getdata_new.m
@@ -0,0 +1,31 @@
+function Outstruct = dnf_getdata_new(Instruct)
+%Section 2F
+%The function receieves a structure containing the defualt parameters for the simulation, and allows the user to replace them with new ones that will be inserted into the output structure.
+%Input: A structure as described in 1)E)i,containing the default parameters of the simulation. The structure contains the following fields:
+%   maxSteps - Maximal number of timepoints, numRepeats - number of realizations for the simulation, minSize - minimal population size serving as a threshold below which the simulation will stop
+%   N0 - the initial population size for both species, Fk - carrying capacity for both species
+
+Titles = {'Maximal number of timepoints', 'Number of numRepeats','Minimal population size', 'N0', 'Fk'};
+Defaults = {num2str(Instruct.maxSteps), num2str(Instruct.numRepeats), num2str(Instruct.minSize), num2str(Instruct.N0), num2str(Instruct.Fk)};
+Dims = [1, 30];
+while true
+    Ans = inputdlg(Titles,'Simulations Data', Dims, Defaults); %display a dialog box where the user inserts values for the different fields
+    Outstruct = struct('maxSteps', str2num(Ans{1}), 'numRepeats', str2num(Ans{2}),...
+        'minSize', str2num(Ans{3}), 'N0', str2num(Ans{4}), 'Fk', str2num(Ans{5}));
+    C=struct2cell(Outstruct); %arrange the struct data as a cell
+    if all(~cellfun(@isempty, C)) && all([C{:}]>=0) %if all the inputs were inserted properly
+        break
+    else % if the user inserted a negative/not numeric input
+        Defaults=Ans; % in case the user corrected only one of a few illegal fields
+        idx=[find(cellfun(@isempty, C));findNeg(C)]; %find illegal inputs
+        [Defaults(idx)]={'error: please insert positive numbers only'}; %display an error message
+    end
+end
+
+function ind=findNeg(x)
+%internal function to find negative indeces. 
+C=cellfun(@(y)(any(y < 0)),x,'Uni',0); %a logical array, with 1 for negative numbers and 0 for positive. we use any because part of the cell is logical and part is not. 
+C=cell2mat(C);
+ind=find(C==1);
+
+
--- populations.m
+++ populations.m
@@ -11,19 +11,20 @@ dnf_Annotate(fileData) %v
 %section E
 [pop1Params,error1Params,cIdx1]=dnf_calcSepParams(fileData.Pop1,0.7); %i
 [pop2Params,error2Params,cIdx2]=dnf_calcSepParams(fileData.Pop2,0.7); %ii
-K1=pop1Params.K; K2=pop2Params.K; %k values (i+ii); 
+K1=pop1Params.K; K2=pop2Params.K; %k values (i+ii);
 sepPopParams=[pop1Params, pop2Params]; %estimated paramaters for both populations (given separate populations)
-sepPopCI=[error1Params,error2Params]; %error values for both populations, given separate populations. 
-S_idx=max([cIdx1 cIdx2]); %max stabilization index  
+sepPopCI=[error1Params,error2Params]; %error values for both populations, given separate populations.
+S_idx=max([cIdx1 cIdx2]); %max stabilization index
 %%
 %section H
 twoCompPops=fileData.Comp; %i
-[compPop1Params, compPop1Error] = dnf_calcCompParams(pop1Params,error1Params,twoCompPops(:,2),...
-    twoCompPops(:,3),twoCompPops(:,1),S_idx);%ii
+[compPop1Params, compPop1Error] = dnf_calcCompParams(pop1Params,error1Params,twoCompPops(:,3),...
+    twoCompPops(:,1),twoCompPops(:,2),S_idx);%ii
 [compPop2Params, compPop2Error] = dnf_calcCompParams(pop2Params,error2Params,twoCompPops(:,3),...
     twoCompPops(:,2),twoCompPops(:,1),S_idx);%iii
 estCompParams=[compPop1Params,compPop2Params]; %iv
-estCompErrors=[compPop1Error,compPop2Error];
+estCompErrors=[compPop1Error,compPop2Error]; %v
+
 %%
 %section I
 calcEstParam=fileData; %i
@@ -36,7 +37,7 @@ times=linspace(0,maxTimePoint,maxTimePoint); %iii
 pop1Size=dnf_calcLogGrowth(pop1Params,times); %iv - Pop 1
 calcEstParam.Pop1=[times;pop1Size]';
 pop2Size=dnf_calcLogGrowth(pop2Params,times); %Pop 2
-calcEstParam.Pop2=[times;pop2Size]'; 
+calcEstParam.Pop2=[times;pop2Size]';
 compSimParams=struct('maxSteps',maxTimeComp,'numRepeats',1,'minSize',0,'N0',[estCompParams.N0],'Fk',1); %v
 %%
 %section J
@@ -67,13 +68,78 @@ compN0_txt2=param2Txt('N0 2nd',estCompParams(2).N0,estCompErrors(2).N0);
 compA_txt1=param2Txt('alpha 1st',estCompParams(1).alpha,estCompErrors(1).alpha);
 compA_txt2= param2Txt('alpha 2nd',estCompParams(2).alpha,estCompErrors(2).alpha);
 lgnd_bottom={fileData.Sp1,fileData.Sp2,[compN0_txt1,compA_txt1],...
-[compN0_txt2,compA_txt2]};
+    [compN0_txt2,compA_txt2]};
 subplot(2,1,2)
 legend(lgnd_bottom)
-
+%%
 %QUESTION 4
 %section A
 fig2=figure; %i
-compParams=dnf_getdata(compSimParams); %ii
+userSimParams=dnf_getdata_new(compSimParams); %ii
 
+% sectionB
 
+while all(userSimParams.N0~=0)
+    userPopParams=estCompParams; %i
+    lenN0=length(userSimParams.N0); %ii - update N0
+    if lenN0>1 %if there is only one value for N0, we copy the value to both populations
+        userPopParams(1).N0=userSimParams.N0(1);
+        userPopParams(2).N0=userSimParams.N0(2);
+    else
+        userPopParams(1).N0=userSimParams.N0;
+        userPopParams(2).N0=userSimParams.N0;
+    end
+    userPopParams(1).K=userPopParams(1).K*userSimParams.Fk;%iii - update K 1st pop
+    userPopParams(2).K=userPopParams(2).K*userSimParams.Fk; %iii - 2nd pop
+    userResults=dnf_knownSim(userPopParams,userSimParams); %iv - simulation
+    clf %v - clean figure
+    %vi
+    sp1_txt=[fileData.Sp1,' N0=',num2str(userPopParams(1).N0),', K=',num2str(userPopParams(1).K)];
+    sp2_txt=[fileData.Sp2,' N0=',num2str(userPopParams(2).N0),', K=',num2str(userPopParams(2).K)];
+    if userSimParams.numRepeats==1
+        plot(userResults.Times,userResults.Pop1);
+        hold on
+        plot(userResults.Times,userResults.Pop2);
+        title(fileData.Title);
+        legend({sp1_txt,sp2_txt});
+        xlabel(fileData.Time)
+    else
+        subplot(2,1,1)
+        plot(userResults.Times,userResults.Pop1)
+        ylabel(sp1_txt)
+        xlabel(fileData.Time)
+        title(fileData.Title);
+        subplot(2,1,2)
+        plot(userResults.Times,userResults.Pop2)
+        ylabel(sp2_txt)
+        xlabel(fileData.Time)
+    end
+    userSimParams=dnf_getdata_new(compSimParams); %viic
+end
+%%
+%section C
+N=size(userResults.Pop1,1);
+if N>1
+    fig3=figure;
+    hold all
+    mPop1=mean(userResults.Pop1,2);
+    sPop1=std(userResults.Pop1,0,2);
+    mPop2=mean(userResults.Pop2,2);
+    sPop2=std(userResults.Pop2,0,2);
+    if N<=50
+        errorbar(userResults.Times,mPop1,sPop1);
+        errorbar(userResults.Times,mPop2,sPop2);
+    else
+        plot(userResults.Times,mPop1)
+        plot(userResults.Times,mPop2)
+        mPop1_P=mPop1(1:5:end); %choose every 5th data point
+        sPop1_P=sPop1(1:5:end);
+        mPop2_P=mPop2(1:5:end);
+        sPop2_P=sPop2(1:5:end);
+        t_P=userResults.Times(1:5:end);
+        errorbar(t_P,mPop1_P,sPop1_P);
+        errorbar(t_P,mPop2_P,sPop2_P);
+        legend({sp1_txt,sp2_txt});
+        suptitle(fileData.Title);
+    end
+end
