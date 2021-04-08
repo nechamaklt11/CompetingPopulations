2557b6810164891f9fd7163b76629f25e730b6fb
Dor-Breier <77353410+Dor-Breier@users.noreply.github.com>
2021-04-09 00:37:04 +0300

    Update dnf_calcLogGrowth.m

--- dnf_calcLogGrowth.m
+++ dnf_calcLogGrowth.m
@@ -2,12 +2,10 @@ function popSize=dnf_calcLogGrowth(params,t)
 %QUESTION 2 SECTION H
 
 %the function calculates population size to a given population in during
-%different time points. The data for each population is given in the
-%"params" struct. 
+%different time points. The data for each population is given in the "params" struct. 
 %Inputs: params - contains the data for each population. The struct has
 %          contains fields as described in section 1Eii: N0 (inital population), K (carrying capacity), lamda (growth factor), alpha (other species effect) 
-%        t - given time points for which population size will be
-%        calculated.
+%        t - given time points for which population size will calculated.
 %Output: popSize - the population size for any given time point. 
 popSize=((params.N0).*((params.lamda).^t))./(1+((params.N0).*((params.lamda).^t-1))./(params.K));
 
